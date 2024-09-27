0245d941c1373293cb68405f2cbeb07082d513b1
Krishanu Chakraborty <krishanuc1001@users.noreply.github.com>
2024-09-27 14:22:57 +0530

    Add E2E test for "Add to Cart" scenario with special locators and organize locator usage in Playwright tests.

--- tests/E2EAddToCart.spec.js
+++ tests/E2EAddToCart.spec.js
@@ -46,7 +46,6 @@ test("E2E Scenario: Add to Cart", async ({browser}) => {
         }
     }
 
-    // await page.pause();
     await cartLocator.click();
     await productList.first().waitFor();
 
@@ -94,3 +93,87 @@ test("E2E Scenario: Add to Cart", async ({browser}) => {
     expect(orderID.includes((await orderSummaryOrderID.textContent()).trim())).toBeTruthy();
 
 });
+
+test.only("E2E Scenario: Add to Cart | Special locators", async ({browser}) => {
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    const URL = "https://rahulshettyacademy.com/client";
+    const email = "anshika@gmail.com";
+    const password = "Iamking@000";
+    const emailLocator = page.getByPlaceholder("email@example.com");
+    const passwordLocator = page.getByPlaceholder("enter your passsword");
+    const loginBtn = page.getByRole('button', {name: "Login"});
+    const products = page.locator("//div[@class='card-body']");
+    const productName = "IPHONE 13 PRO";
+    const cartLocator = page.locator("//li").getByRole("button", {name: "Cart"});
+    const productList = page.locator("//div//li");
+    const productNameLocator = page.getByText(productName);
+    const countryOptionsDropdown = page.locator("//section[contains(@class, 'ta-results')]");
+    const selectCountry = page.getByPlaceholder("Select Country");
+
+    const orderHistoryPage = page.locator("//td[@class='em-spacer-1']//label[@routerlink='/dashboard/myorders']");
+    const checkoutBtn = page.getByRole("button", {name: "Checkout"});
+    const expiryMonth = page.locator("(//select[@class='input ddl'])[1]");
+    const expiryYear = page.locator("(//select[@class='input ddl'])[2]");
+    const cvv = page.locator("//div[contains(text(), 'CVV Code')]//following-sibling::input");
+    const nameOnCard = page.locator("//div[contains(text(), 'Name on Card ')]//following-sibling::input");
+    // const emailLabel = page.getByLabel(email);
+    const emailLabel = page.locator("//div[contains(@class, 'user__name')]//label");
+    const placeOrder = page.getByText("Place Order ");
+    const thankYouText = page.getByText("Thankyou for the order.");
+    const orderIDLocator = page.locator("//td[@class='em-spacer-1']//label[@class='ng-star-inserted']");
+    const rows = page.locator("//tbody//tr");
+    const orderSummaryOrderID = page.locator("//div[@class='col-text -main']");
+
+    await page.goto(URL);
+    await emailLocator.fill(email);
+    await passwordLocator.fill(password);
+    await loginBtn.click();
+
+    await products.first().waitFor();
+    // await page.waitForLoadState('networkidle');
+    await products.filter({hasText: productName})
+        .getByRole("button", {name: "Add To Cart"})
+        .click();
+
+    await cartLocator.click();
+    await productList.first().waitFor();
+
+    expect(await productNameLocator.isVisible()).toBeTruthy();
+
+    await checkoutBtn.click();
+
+    // Fill the credit card details
+    await expiryMonth.selectOption("11");
+    await expiryYear.selectOption("27");
+    await cvv.fill('123');
+    await nameOnCard.fill('Anshika');
+
+    await selectCountry.pressSequentially("ind");
+    await page.getByRole('button', {name: "India"}).nth(1).click();
+
+    await expect(emailLabel).toHaveText(email);
+
+    await placeOrder.click();
+
+    await expect(thankYouText).toBeVisible();
+
+    const orderID = await orderIDLocator.textContent();
+    console.log(orderID);
+
+    await orderHistoryPage.click();
+    await page.locator("//tbody").waitFor();
+
+    for (let i = 0; i < (await rows.count()); i++) {
+        const rowOrderId = rows.nth(i).locator("//th").textContent();
+        if (orderID.includes(await rowOrderId)) {
+            console.log("Order ID found in the order history");
+            await rows.nth(i).locator("//button[@class='btn btn-primary']").click();
+            break;
+        }
+    }
+
+    expect(orderID.includes((await orderSummaryOrderID.textContent()).trim())).toBeTruthy();
+
+})
+;
