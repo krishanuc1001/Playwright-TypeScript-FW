44b03a44b0c21dacfa7cdf6ee4085c81f142cd78
Krishanu Chakraborty <krishanuc1001@users.noreply.github.com>
2025-02-09 17:43:26 +0530

    Add comment on storing token and orderId

--- tests/utils/APIUtils.js
+++ tests/utils/APIUtils.js
@@ -20,7 +20,7 @@ class APIUtils {
 
     async createOrder(orderPayload) {
         let response = {};
-        response.token = await this.getToken();
+        response.token = await this.getToken(); // Storing token to the response object for later use in the test script
         const orderResponse = await this.apiContext.post("https://rahulshettyacademy.com/api/ecom/order/create-order", {
             data: orderPayload,
             headers: {
@@ -32,7 +32,7 @@ class APIUtils {
         const orderResponseJSON = await orderResponse.json();
         console.log("Order Response: " + orderResponseJSON);
         const orderId = orderResponseJSON.orders[0];
-        response.orderId = orderId;
+        response.orderId = orderId; // Storing order ID to the response object for later use in the test  script
         console.log("Order ID: " + orderId);
         return response;
     }
