661370d1bc54e8cbfb3836f2b795c26f23096d8a
Krishanu Chakraborty <krishanuc1001@users.noreply.github.com>
2025-05-29 14:02:59 +0530

    Refactor APIUtils class to include orderPayload in constructor and improve token handling

--- tests/WebAPI.spec.js
+++ tests/WebAPI.spec.js
@@ -1,40 +1,50 @@
-const {test, expect, request, Page} = require('@playwright/test');
-const {APIUtils} = require('./utils/APIUtils');
-const loginPayload = {userEmail: "anshika@gmail.com", userPassword: "Iamking@000"};
-const orderPayload = {orders: [{country: "India", productOrderedId: "6581cade9fd99c85e8ee7ff5"}]};
+const { test, expect, request, Page } = require("@playwright/test");
+const { APIUtils } = require("./utils/APIUtils.js");
+const loginPayload = {
+  userEmail: "anshika@gmail.com",
+  userPassword: "Iamking@000",
+};
+const orderPayload = {
+  orders: [{ country: "India", productOrderedId: "67a8df56c0d3e6622a297ccd" }],
+};  
 let response;
 
-test.beforeAll("Get token", async () => {
-    const apiContext = await request.newContext();
-    const apiUtils = new APIUtils(apiContext, loginPayload);
-    response = await apiUtils.createOrder(orderPayload);
+test.beforeAll("Skip login by fetching token and Create Order", async () => {
+  const apiContext = await request.newContext();
+  const apiUtils = new APIUtils(apiContext, loginPayload, orderPayload); 
+  response = await apiUtils.createOrder(orderPayload);
 });
 
-test("Web and API test with Playwright", async ({page}) => {
+// This test will use the token and orderId from the API response to interact with the web application
+// We will verify that the order created via API is visible in the web application
+test("Web and API test with Playwright", async ({ page }) => {
+  
+  // Use addInitScript to set the token in localStorage, this allows to use Javascript method window.localStorage.setItem 
+  // This allows the page to access the token as if it was set by the browser
+  await page.addInitScript((value) => {
+    window.localStorage.setItem("token", value); 
+  }, response.token);
 
-    await page.addInitScript(value => {
-        window.localStorage.setItem("token", value);
-    }, response.token);
+  await page.goto("https://rahulshettyacademy.com/client/");
+  const orders = page.locator("//button[@routerlink='/dashboard/myorders']");
+  const orderSummaryOrderID = page.locator("//div[@class='col-text -main']");
+  const orderTable = page.locator("//tbody");
+  const orderRows = page.locator("//tbody//tr");
 
-    await page.goto("https://rahulshettyacademy.com/client/");
-    const orders = page.locator("//button[@routerlink='/dashboard/myorders']");
-    const orderSummaryOrderID = page.locator("//div[@class='col-text -main']");
-    const orderTable = page.locator("//tbody");
-    const orderRows = page.locator("//tbody//tr");
+  await orders.click();
+  await orderTable.waitFor();
 
-    await orders.click();
-    await orderTable.waitFor();
-
-    for (let i = 0; i < await orderRows.count(); i++) {
-        const rowOrderId = await orderRows.nth(i).locator("th").textContent();
-        if (response.orderId.includes(rowOrderId)) {
-            await orderRows.nth(i).locator("button").first().click();
-            break;
-        }
+  for (let i = 0; i < (await orderRows.count()); i++) {
+    const rowOrderId = await orderRows.nth(i).locator("th").textContent();
+    if (response.orderId.includes(rowOrderId)) {
+      await orderRows.nth(i).locator("button").first().click();
+      break;
     }
+  }
 
-    const orderIdDetails = await orderSummaryOrderID.textContent();
-    expect(response.orderId.includes(orderIdDetails)).toBeTruthy();
+  const orderIdDetails = await orderSummaryOrderID.textContent();
+  page.pause();
+  expect(response.orderId.includes(orderIdDetails)).toBeTruthy();
 
-    await orders.click();
+  await orders.click();
 });
--- tests/utils/APIUtils.js
+++ tests/utils/APIUtils.js
@@ -1,42 +1,60 @@
-const {expect} = require("@playwright/test");
+import { expect } from "@playwright/test";
 
 class APIUtils {
+  /**
+   * Constructor for APIUtils class.
+   * @param {APIRequestContext} apiContext - The API request context to be used for making requests.
+   * @param {Object} loginPayload - The payload containing user credentials for login.
+   * @param {Object} orderPayload - The payload containing order details.
+   */
+  constructor(apiContext, loginPayload, orderPayload) {
+    this.apiContext = apiContext;
+    this.loginPayload = loginPayload;
+    this.orderPayload = orderPayload;
+  }
 
-    constructor(apiContext, loginPayload) {
-        this.apiContext = apiContext;
-        this.loginPayload = loginPayload;
-    }
+  async getToken() {
+    const loginResponse = await this.apiContext.post(
+      "https://rahulshettyacademy.com/api/ecom/auth/login",
+      {
+        data: this.loginPayload,
+      }
+    );
 
-    async getToken() {
-        const loginResponse = await this.apiContext.post("https://rahulshettyacademy.com/api/ecom/auth/login", {
-            data: this.loginPayload
-        });
+    // Check if the response status is 200 or ok
+    expect(loginResponse.ok()).toBeTruthy(); // Ensure the response is successful
 
-        const loginResponseJSON = await loginResponse.json();
-        const loginToken = loginResponseJSON.token;
-        console.log(loginToken);
-        return loginToken;
-    }
+    // if (loginResponse.statusText !== 200) {
+    //   throw new Error("Failed to log in: " + loginResponse.statusText());
+    // }
+    const loginResponseJSON = await loginResponse.json();
+    const loginToken = loginResponseJSON.token;
+    console.log(loginToken);
+    return loginToken;
+  }
 
-    async createOrder(orderPayload) {
-        let response = {};
-        response.token = await this.getToken(); // Storing token to the response object for later use in the test script
-        const orderResponse = await this.apiContext.post("https://rahulshettyacademy.com/api/ecom/order/create-order", {
-            data: orderPayload,
-            headers: {
-                "Authorization": response.token,
-                "Content-Type": "application/json"
-            }
-        });
+  async createOrder(orderPayload) {
+    let response = {};
+    response.token = await this.getToken(); // Storing token to the response object for later use in the test script
+    const orderResponse = await this.apiContext.post(
+      "https://rahulshettyacademy.com/api/ecom/order/create-order",
+      {
+        data: orderPayload,
+        headers: {
+          Authorization: response.token,
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
-        const orderResponseJSON = await orderResponse.json();
-        console.log("Order Response: " + orderResponseJSON);
-        const orderId = orderResponseJSON.orders[0];
-        response.orderId = orderId; // Storing order ID to the response object for later use in the test  script
-        console.log("Order ID: " + orderId);
-        return response;
-    }
+    const orderResponseJSON = await orderResponse.json();
+    console.log(orderResponseJSON);
 
+    const orderId = orderResponseJSON.orders[0];
+    response.orderId = orderId; // Storing order ID to the response object for later use in the test script
+    console.log("Order ID: " + orderId);
+    return response;
+  }
 }
 
-module.exports = {APIUtils};
+export { APIUtils };
