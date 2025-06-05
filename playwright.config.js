50c8063ddf64147717d53fc4a30373f60355c8cd
Krishanu Chakraborty <krishanuc1001@users.noreply.github.com>
2025-06-06 00:11:06 +0530

    Update Playwright config to set trace option to 'on' for better debugging

--- playwright.config.js
+++ playwright.config.js
@@ -32,7 +32,7 @@ module.exports = defineConfig({
         // baseURL: 'http://127.0.0.1:3000',
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-        trace: "retain-on-failure",
+        trace: 'on',
 
         headless: false,
         screenshot: "on"
