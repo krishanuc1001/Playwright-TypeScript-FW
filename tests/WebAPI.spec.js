912ac4f977d2e0fb9f1edd05145eea01fe81784b
Krishanu Chakraborty <krishanuc1001@users.noreply.github.com>
2025-06-20 12:49:06 +0530

    Fix formatting inconsistencies in WebAPI.spec.js

--- tests/WebAPI.spec.js
+++ tests/WebAPI.spec.js
@@ -6,23 +6,23 @@ const loginPayload = {
 };
 const orderPayload = {
   orders: [{ country: "India", productOrderedId: "67a8df56c0d3e6622a297ccd" }],
-};  
+};
 let response;
 
 test.beforeAll("Skip login by fetching token and Create Order", async () => {
   const apiContext = await request.newContext();
-  const apiUtils = new APIUtils(apiContext, loginPayload, orderPayload); 
+  const apiUtils = new APIUtils(apiContext, loginPayload, orderPayload);
   response = await apiUtils.createOrder(orderPayload);
 });
 
 // This test will use the token and orderId from the API response to interact with the web application
 // We will verify that the order created via API is visible in the web application
+// The test uses `addInitScript` to set the token in localStorage, allowing the web application to access it as if it was set by the browser.
 test("Web and API test with Playwright", async ({ page }) => {
-  
-  // Use addInitScript to set the token in localStorage, this allows to use Javascript method window.localStorage.setItem 
+  // Use addInitScript to set the token in localStorage, this allows to use Javascript method window.localStorage.setItem
   // This allows the page to access the token as if it was set by the browser
   await page.addInitScript((value) => {
-    window.localStorage.setItem("token", value); 
+    window.localStorage.setItem("token", value);
   }, response.token);
 
   await page.goto("https://rahulshettyacademy.com/client/");
